80ace7392d4be3a979a58ed71ed26916bbd05e32
[vengadaganapathy1] <upskilltechnology24@gmail.com>
2024-05-28 13:24:06 +0530

    Updated codes

---
+++ using-jsx/main.js
@@ -0,0 +1,105 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function createHeader() {
+  const header = React.createElement(
+    "h1",
+    {},
+    "This is a sample header component"
+  );
+  const rootEle = ReactDOM.createRoot(document.getElementById("root"));
+  rootEle.render(header);
+}
+createHeader();
+
+const headingUsingJSX = (
+  <div>
+    <h1 tabIndex='1' className='header'>
+      This is a header component using JSX
+    </h1>
+    <h1 className='header'>This is a another header component using JSX</h1>
+  </div>
+);
+
+function createHeaderUsingJSX() {
+  const rootEle = ReactDOM.createRoot(document.getElementById("rootUsingJSX"));
+  rootEle.render(headingUsingJSX);
+}
+createHeaderUsingJSX();
+
+const HeaderComponent = () => {
+  return (
+    <header>
+      <h1>Header {Math.random()}</h1>
+    </header>
+  );
+};
+
+const FooterComponent = () => {
+  return (
+    <footer>
+      <h1>Footer</h1>
+    </footer>
+  );
+};
+
+const NavComponent = () => {
+  return (
+    <ul>
+      <li>Menu 1</li>
+      <li>Menu 2</li>
+      <li>Menu 3</li>
+      <li>Menu 4</li>
+    </ul>
+  );
+};
+
+const MainComponent = () => {
+  return (
+    <div>
+      <h3>Image gallery</h3>
+      <div className='image-container'>
+        <ImageComponent />
+        <ImageComponent />
+        <ImageComponent />
+        <ImageComponent />
+        <ImageComponent />
+      </div>
+    </div>
+  );
+};
+
+const ImageComponent = () => {
+  return (
+    <div>
+      <img
+        src={require("../assets/react.png")}
+        width='100'
+        height='100'
+        alt='img'
+      />
+    </div>
+  );
+};
+
+function createPageUsingFunctionalComp() {
+  const page = (
+    <div>
+      <div>
+        <HeaderComponent></HeaderComponent>
+      </div>
+      <div>{NavComponent()}</div>
+      <div>
+        <MainComponent />
+      </div>
+      <div>
+        <FooterComponent />
+      </div>
+    </div>
+  );
+  const rootEle = ReactDOM.createRoot(
+    document.getElementById("rootForFunctionalComponents")
+  );
+  rootEle.render(page);
+}
+createPageUsingFunctionalComp();
